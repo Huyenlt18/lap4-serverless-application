1fe4d0ae30a2a63e084539b1041b3cc6e79ef798
Huyenlt18 <57925448+Huyenlt18@users.noreply.github.com>
2022-08-07 10:17:13 +0700

    Initial commit

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,42 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils';
+import { createTodo } from '../../businessLogic/todos'
+import { createLogger } from "../../utils/logger";
+const logger = createLogger("create-todo");
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+      const newTodo: CreateTodoRequest = JSON.parse(event.body)
+      // TODO: Implement creating a new TODO item
+      const userId = getUserId(event)
+      const item = await createTodo(userId, newTodo)
+      return {
+        statusCode: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          item
+        })
+      }
+
+    } catch (e) {
+      logger.error(e.message)
+      return {
+        statusCode: 500,
+        body: e.message
+      }
+    }
+
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
---
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -0,0 +1,47 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+import { createLogger } from "../../utils/logger";
+const logger = createLogger("generateUpload-todo");
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+    const userId = getUserId(event)
+    try {
+      const uploadUrl = await createAttachmentPresignedUrl(todoId, userId)
+
+      logger.info('Upload URL: ' + uploadUrl)
+
+      return {
+        statusCode: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          uploadUrl: uploadUrl
+        })
+      }
+    } catch (e) {
+      logger.error(e.message)
+      return {
+        statusCode: 500,
+        body: e.message
+      }
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
